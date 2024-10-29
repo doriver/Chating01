70a120dbaac0d09a260aeceb099b87d2d2ac3790
DESKTOP-5360O1P\server <tkdduq241@naver.com>
2024-10-29 14:03:54 +0900

    채팅 프로젝트 시작

---
+++ src/test/java/com/example/demo/Chating01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chating01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
